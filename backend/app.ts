bf2d4be4f1e03aab52cd19920150bcb1870d68f7
nkmiller7 <nkmiller78787@gmail.com>
2026-03-09 14:26:38 -0400

    Inital commit for backend.

---
+++ backend/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
+});
