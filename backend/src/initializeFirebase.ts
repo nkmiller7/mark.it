f8f4a60af7782f1e1b3b718a741adb8df5326545
nkmiller7 <nkmiller78787@gmail.com>
2026-04-01 14:38:17 -0400

    Fixed CI/CD build pipeline.

--- backend/src/initializeFirebase.ts
+++ backend/src/initializeFirebase.ts
@@ -2,7 +2,7 @@ import { initializeApp, App } from "firebase-admin/app";
 import admin from "firebase-admin";
 
 const firebaseApp: App = initializeApp({
-    credential: admin.credential.cert(String(process.env.FIREBASE_ADMIN_SDK)),
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_SDK)),
 });
 
 export { firebaseApp };
